036e62a3ecab6ea111f403a60294b3e22578fc3c
QuinlanVic <109174553+QuinlanVic@users.noreply.github.com>
2024-02-20 13:09:22 +0000

    list exercises done

--- list.py
+++ list.py
@@ -1,29 +1,58 @@
 # marks = [98, 75, 40, 45, 80, 60]
 # print(type(marks))
-# # marks.pop() # take the last value from the array
+# # marks.pop() # remove the last value from the array
 # print(marks)
 # print(marks[:-3]) # does not mutate the list
 # print(len(marks))
 # # print(marks.pop(-3)) # mutates the list
 # print(marks)
 
-marks = [98, 75, 40, 45, 80, 60]
-marks.remove(40) # mutates the list
-print(marks)
+# marks = [98, 75, 40, 45, 80, 60]
+# marks.remove(40) # mutates the list (remove specific value) (not index)
+# print(marks)
 
-eng = 88
-marks.append(eng) # mutates the list (adds to the end)
-print(marks)
+# eng = 88
+# marks.append(eng) # mutates the list (adds to the end)
+# print(marks)
 
-sci = 85
-marks.insert(2, sci)
-print(marks)
+# sci = 85
+# marks.insert(2, sci) # add at specific position/index in list
+# print(marks)
 
-# [5, 6] * 2 -> [5, 6, 5, 6]
-price_list1 = [1000, 1500, 400]
-price_list2 = [2000, 500]
+# [5, 6] * 2 -> [5, 6, 5, 6] (Duplicate list)
+# price_list1 = [1000, 1500, 400]
+# price_list2 = [2000, 500]
 
 # price_list3 = price_list1 + price_list2 
 # print(price_list3)
-print(price_list1 + price_list2) # does not mutate list
-print(price_list1, price_list2)
+# print(price_list1 + price_list2) # does not mutate list
+# print(price_list1, price_list2)
+
+# price_list1_copy = price_list1
+# price_list1_copy.append(600)
+# price_list1.append(700)
+# print(price_list1, price_list1_copy)
+
+# price_list1 = stores the memory address of the first item in the list (e.g. 456)
+# 456 + 0 = first list item
+# 456 + 1 = second list item
+# 456 + 2 = third list item
+
+# Slice -> copy
+price_list = [1000, 1500, 400]
+price_list2 = price_list[:]
+# price_list2 = price_list.copy()
+
+price_list2.append(600)
+price_list.append(700)
+print(price_list, price_list2)
+print(id(price_list), id(price_list2))
+
+subjects = ["maths", "science", "eng"]
+print(", ".join(subjects)) # returns a string
+
+subjects.sort() # mutates list
+print(subjects)
+subjects.sort(reverse=True)
+print(subjects)
+
