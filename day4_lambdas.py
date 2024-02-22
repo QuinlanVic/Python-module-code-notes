79a0d7695ff9fc9d7ba59d655749fc34d528a58a
Quinlan <109174553+QuinlanVic@users.noreply.github.com>
2024-02-22 14:43:12 +0000

    last lambda additions

--- day4_lambdas.py
+++ day4_lambdas.py
@@ -46,3 +46,63 @@ print(player_stats)
 
 # map object -> lazy | efficient
 
+def sayHello1():
+  def msg():
+    return "Hello, 🎊"
+  return msg 
+
+# Task 1
+msg_function = sayHello1()
+print(msg_function())
+
+# Task 2
+print(sayHello1()())
+
+# Task 3
+# Anything on the right of the ":" is returned by the lambda function
+# In this case another function is returned | Implicit return | Factory function
+# Programming paradigm = Functional programming (currying)
+mul = lambda x: lambda y: x * y
+
+# print(type(mul(5))) # type function
+# mul 5 = lambda y: 5 * y
+# 3 6 -> 18
+# first application accepts 3, second application of returned function accepts 6
+print(mul (3)(6))
+# lambda x is returning lambda y as a function
+# x -> y -> x * y
+mul_1 = mul (3)
+print(mul_1(6))
+
+# Higher Order Function (HOF) - Argument = function
+result1 = map(lambda x: x * 2, [10, 30, 60])
+result2 = filter(lambda x: x > 10, [0, 50, 60, 100, 6, 8, 30])
+print(list(result1))
+print(list(result2))
+
+# Sequence - List
+
+print(sum([10, 30, 60]))
+print(max([10, 80, 30, 60]))
+print(min([10, -1, 30, 60]))
+
+print(all([True, False, True]))
+print(any([True, False, True]))
+
+print(all([10, 0, 30, -1]))
+
+# Falsy values | When converted to Boolean = False
+# 1. 0
+# 2. []
+# 3. None
+# 4. {}
+# 5. ""
+# 6. set()
+# 7. ()
+# 8. False
+
+x = []
+if (x):
+  print('cool')
+else:
+  print('super')
