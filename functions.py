cbf43dd479c7276dc709388b79f1de2ed2a9037b
Quinlan <109174553+QuinlanVic@users.noreply.github.com>
2024-02-22 07:25:43 +0000

    first functions lesson till 1st break

---
+++ functions.py
@@ -0,0 +1,48 @@
+# a = 8
+# b = 10
+
+# print("The sum is: ", a + b)
+
+# a1 = 60
+# b1 = 70
+
+# print("the sum is: ", a1 + b1)
+
+# a2 = 600
+# b2 = 70
+
+# print("The sum is: ", a2 + b2)
+
+# Functions are a way to pack your logic
+# Declaration / Definition
+# sum = function name
+# a and b are the 2 parameters
+def sum(a, b): 
+  # a + b # None
+  return a + b # body
+
+# Abstracted the logic | Hiding implementation
+
+print("the sum is: ", sum(30, 50))
+
+# Default values!!!!
+#default value if argument not given
+# All default values have to be at the end/last. Cannot have default values for
+# arguments before and not have after
+def driving_test(name, age=15, car="Toyota tazz"): 
+  # print(age)
+  if age >= 18:
+    return f"{name} you're eligible for driving. You will do the test with a {car}"
+  else:
+    return "Try again after a few years :)"
+
+# print(driving_test("Caleb", 20, "GR Corolla"))
+# print(driving_test(10))
+# print(driving_test("Gemma", 20))
+
+# Types of argument
+# Position argument
+# Keyword arguments!!!!
+print(driving_test(age=21, name="Tina")) 
+print(driving_test("Gemma"))
+# [9, 3, 8].sort(reverse=True)
