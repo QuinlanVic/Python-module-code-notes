6ca5ef411971c28c728ec6d7e4eb1f7e45779aa3
QuinlanVic <Quinlanvc@gmail.com>
2024-03-18 17:05:00 +0200

    end of work today

--- day19_async_api.py
+++ day19_async_api.py
@@ -172,6 +172,10 @@ async def get_city_name_temp(city_name):
 
 #     print("Task 4")
 #     # NO - does one by one and slow
+#     # print(await get_city_name_temp("New York"))
+#     # print(await get_city_name_temp("Hong Kong"))
+#     cities_data = [await get_city_name_temp(city) for city in cities]
+#     pprint(dict(cities_data))
 
 #     # Performance
 #     cities_data_concurrent = [get_city_name_temp(city) for city in cities]
@@ -211,6 +215,109 @@ async def main():
     print(await get_names(names_url))
 
 
-asyncio.run(main())
+# asyncio.run(main())
 
 # params
+# have to put data in the body (POST and PUT requests) (safer)
+# mockapi autogenerates created at and id values
+# postman used to test if api is working
+
+
+# Delete user with id = 15
+async def delete_user_by_id(id):
+    url = f"https://65f8286cb4f842e8088713ab.mockapi.io/users/{id}"
+    async with aiohttp.ClientSession() as session:
+        async with session.delete(url) as response:
+            user = await response.json()
+            # print(users)
+    return user
+
+
+async def main(id):
+    user = await delete_user_by_id(id)
+    pprint(user)
+
+
+# asyncio.run(main(10))
+
+# Task - 1
+# Delete the first 5 users
+# Performant < 0.5s
+
+#     # Performance
+#     cities_data_concurrent = [get_city_name_temp(city) for city in cities]
+#     cities_data = await asyncio.gather(*cities_data_concurrent)
+#     pprint(dict(cities_data))
+
+
+async def get_first_five_users(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            users = await response.json()
+            # print(users)
+    return [user["id"] for user in users[0:5]]
+
+
+async def main2():
+    names_url = "https://65f8286cb4f842e8088713ab.mockapi.io/users"
+    # first_five_ids = [1, 2, 4, 5, 6]
+    first_five_ids = await get_first_five_users(names_url)
+    print(first_five_ids)
+    users_data_concurrent = [delete_user_by_id(id) for id in first_five_ids]
+    users_data = await asyncio.gather(*users_data_concurrent)
+    pprint(users_data)
+
+
+# async speedup!
+# start_time = time()
+# asyncio.run(main2())
+# end_time = time()
+
+# print(f"Time taken: {end_time - start_time}")
+
+
+# Task 2
+# Change all the users' avatars to human rights flag
+
+
+async def get_all_users_id(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            users = await response.json()
+            # print(users)
+    return [user["id"] for user in users]
+
+
+import json
+
+
+async def update_avatar(id):
+    url = f"https://65f8286cb4f842e8088713ab.mockapi.io/users/{id}"
+    # have to make avatar json
+    avatar = {
+        "avatar": "https://static.vecteezy.com/system/resources/previews/020/297/008/non_2x/south-africa-human-rights-day-march-21-for-greeting-card-poster-banner-template-free-vector.jpg"
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.put(url, data=json.dumps(avatar)) as response:
+            updated_user = await response.json()
+    return updated_user
+
+
+async def main3():
+    print("Main 3 Final")
+    names_url = "https://65f8286cb4f842e8088713ab.mockapi.io/users"
+    # get all the user ids
+    all_users_id = await get_all_users_id(names_url)
+    # get all co-routines (load shotgun)
+    users_data_co_routine = [update_avatar(id) for id in all_users_id]
+    # use "gather" to run all co-routines concurrently (shoot all co-routines)
+    users_data = await asyncio.gather(*users_data_co_routine)
+    # get data that was updated and pretty print it out
+    pprint(users_data)
+
+
+start_time = time()
+asyncio.run(main3())
+end_time = time()
+
+print(f"Time taken: {end_time - start_time}")
